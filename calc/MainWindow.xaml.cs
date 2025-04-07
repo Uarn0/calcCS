e6c480d54990fd1eafcbc73e799074932e455dee
Uarn0 <nazarvyginny@gmail.com>
2025-04-07 04:36:27 +0300

    DO panel

--- calc/MainWindow.xaml.cs
+++ calc/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -19,7 +22,7 @@ public partial class MainWindow : Window
     private bool isResultDisplayed = false;
 
     private object lastResult = "";
-
+    private bool isPanelOpen = false;
     public MainWindow()
     {
 
@@ -46,6 +49,63 @@ public partial class MainWindow : Window
         btnMinus.Click += BtnMinus_Click;
         btnProcent.Click += BtnProcent_Click;
         btnPosNeg.Click += BtnPosNeg_Click;
+        btnMore.Click += BtnMore_Click;
+        btnPi.Click += BtnPi_Click;
+        btnLogariphm.Click += BtnLogariphm_Click;
+        btnEpsilon.Click += BtnEpsilon_Click;
+        btnDegree.Click += BtnDegree_Click;
+        btnSqrt.Click += BtnSqrt_Click;
+    }
+
+    private void BtnSqrt_Click(object sender, RoutedEventArgs e)
+    {
+        txtDisplay.Text += "√";
+    }
+
+
+
+    private void BtnDegree_Click(object sender, RoutedEventArgs e)
+    {
+        txtDisplay.Text += "^";
+    }
+
+    private void BtnEpsilon_Click(object sender, RoutedEventArgs e)
+    {
+        txtDisplay.Text += "e";
+    }
+
+    private void BtnLogariphm_Click(object sender, RoutedEventArgs e)
+    {
+        txtDisplay.Text += "ln";
+    }
+
+    private void BtnPi_Click(object sender, RoutedEventArgs e)
+    {
+        txtDisplay.Text += "π";
+    }
+
+    private void ExpandLastColumn()
+    {
+        LastColumn.Width = new GridLength(1, GridUnitType.Star);
+    }
+    private void CollapseLastColumn()
+    {
+        LastColumn.Width = new GridLength(0);
+    }
+    private void BtnMore_Click(object sender, RoutedEventArgs e)
+    {
+        if (isPanelOpen)
+        {
+            CollapseLastColumn();
+            btnMore.Content = "≡";
+        }
+        else
+        {
+            ExpandLastColumn();
+            btnMore.Content = "⭕";
+        }
+
+        isPanelOpen = !isPanelOpen;
     }
 
     private void BtnPosNeg_Click(object sender, RoutedEventArgs e)
@@ -115,16 +175,41 @@ public partial class MainWindow : Window
 
         }
     }
+    
+
     private void BtnEquals_Click(object sender, RoutedEventArgs e)
     {
         try
         {
             OntxtDisplay.Text = txtDisplay.Text;
 
-            string expression = txtDisplay.Text.Replace(",", ".").Replace("×", "*").Replace("÷", "/");
+            string expression = txtDisplay.Text
+                .Replace(",", ".")
+                .Replace("×", "*")
+                .Replace("÷", "/")
+                .Replace("π", "3.141592653589793")
+                .Replace("e", "2.718281828459045");
 
-            object result = new System.Data.DataTable().Compute(expression, null);
+            expression = Regex.Replace(expression, @"√(\d+(\.\d+)?)", m =>
+            {
+                double number = double.Parse(m.Groups[1].Value);
+                return Math.Sqrt(number).ToString().Replace(",", ".");
+            });
 
+            expression = Regex.Replace(expression, @"ln(\d+(\.\d+)?)", m =>
+            {
+                double number = double.Parse(m.Groups[1].Value);
+                return Math.Log(number).ToString().Replace(",", ".");
+            });
+
+            expression = Regex.Replace(expression, @"(\d+(\.\d+)?)\^(\d+(\.\d+)?)", m =>
+            {
+                double baseNum = double.Parse(m.Groups[1].Value);
+                double exponent = double.Parse(m.Groups[3].Value);
+                return Math.Pow(baseNum, exponent).ToString().Replace(",", ".");
+            });
+
+            var result = new DataTable().Compute(expression, "");
             txtDisplay.Text = result.ToString();
             lastResult = result;
             isResultDisplayed = true;
@@ -134,6 +219,7 @@ public partial class MainWindow : Window
             txtDisplay.Text = "Error";
         }
     }
+
 
     private void BtnMinus_Click(object sender, RoutedEventArgs e)
     {
@@ -305,6 +391,7 @@ public partial class MainWindow : Window
         OntxtDisplay.Text = "";
         txtDisplay.Text = "";
     }
+    
 
     private void BtnUndo_Click(object sender, RoutedEventArgs e)
     {
@@ -313,6 +400,7 @@ public partial class MainWindow : Window
             txtDisplay.Text = txtDisplay.Text.Substring(0, txtDisplay.Text.Length - 1);
         }
     }
+
     //private void BtnClearEntry_Click(object sender, RoutedEventArgs e)
     //{
     //    if (!string.IsNullOrEmpty(txtDisplay.Text))
